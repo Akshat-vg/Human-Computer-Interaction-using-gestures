d575e90eb000d2da52b1fa7e908435a1c3a7daa0
Akshat Ghanathay <93379911+Akshat-vg@users.noreply.github.com>
2024-03-16 12:20:38 +0530

    fix: user data map

--- script/modules/user_def_controls.py
+++ script/modules/user_def_controls.py
@@ -1,11 +1,13 @@
+import json
 import subprocess
-
-import pyautogui
 
 
 class UserDefControls:
     def __init__(self, hand_tracker):
         self.hand_tracker = hand_tracker
+        with open("./user_data.json", "r") as file:
+            self.app_data = json.load(file)
+
         self.data = [
             {
                 "displayName": "Explorer",
@@ -20,21 +22,18 @@ class UserDefControls:
             },
         ]
 
+        self.gesture_map = {
+            "index": [0, 1, 0, 0, 0],
+            "index and middle": [0, 1, 1, 0, 0],
+            "index, middle and ring": [0, 1, 1, 1, 0],
+            "index, middle, ring and little": [0, 1, 1, 1, 1],
+            "thumb": [1, 0, 0, 0, 0],
+        }
 
-def user_controls(self, raised_fingers):
-    if raised_fingers is not None and raised_fingers != [0, 0, 0, 0, 0]:
-        # gesture 1: index
-        if raised_fingers == [0, 1, 0, 0, 0]:
-            subprocess.run(self.data[0]["shellName"], shell=True)
-        # gesture 2: index and middle
-        elif raised_fingers == [0, 1, 1, 0, 0]:
-            subprocess.run(self.data[1]["shellName"], shell=True)
-        # gesture 3: index, middle and ring
-        elif raised_fingers == [0, 1, 1, 1, 0]:
-            subprocess.run(self.data[2]["shellName"], shell=True)
-        # gesture 4: index, middle, ring and little
-        elif raised_fingers == [0, 1, 1, 1, 1]:
-            subprocess.run(self.data[3]["shellName"], shell=True)
-        # gesture 5: thumb
-        elif raised_fingers == [1, 0, 0, 0, 0]:
-            subprocess.run(self.data[4]["shellName"], shell=True)
+    def user_controls(self, raised_fingers):
+        for gesture, shellName in self.app_data["userDefinedControls"].items():
+            if self.gesture_map[gesture] == raised_fingers:
+                for app in self.data:
+                    if app["displayName"] == shellName:
+                        subprocess.run(app["shellName"], shell=True)
+                        break
