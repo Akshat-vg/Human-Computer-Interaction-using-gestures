19d4f26e1984d5dd75a0052a2dcb19cc0185f100
Adithya Kramadhati <a.dty.3403@gmail.com>
2024-03-16 13:31:16 +0530

    Update user-defined controls configuration

--- script/modules/user_def_controls.py
+++ script/modules/user_def_controls.py
@@ -5,35 +5,56 @@ import subprocess
 class UserDefControls:
     def __init__(self, hand_tracker):
         self.hand_tracker = hand_tracker
-        with open("./user_data.json", "r") as file:
+
+        with open("./script/modules/user_defined_data.json", "r") as file:
             self.app_data = json.load(file)
 
-        self.data = [
-            {
-                "displayName": "Explorer",
-                "shellName": ["explorer.exe", "microsoft.windows.camera:"],
-            },
-            {"displayName": "Calculator", "shellName": ["calc.exe"]},
-            {"displayName": "Notepad", "shellName": ["notepad.exe"]},
-            {"displayName": "Settings", "shellName": ["explorer.exe", "ms-settings:"]},
-            {
-                "displayName": "Photos",
-                "shellName": ["explorer.exe", "microsoft.windows.photos:"],
-            },
-        ]
-
         self.gesture_map = {
-            "index": [0, 1, 0, 0, 0],
-            "index and middle": [0, 1, 1, 0, 0],
-            "index, middle and ring": [0, 1, 1, 1, 0],
-            "index, middle, ring and little": [0, 1, 1, 1, 1],
-            "thumb": [1, 0, 0, 0, 0],
+            "[0, 1, 0, 0, 0]": "index",
+            "[0, 1, 1, 0, 0]": "index and middle",
+            "[0, 1, 1, 1, 0]": "index, middle and ring",
+            "[0, 1, 1, 1, 1]": "index, middle, ring and little",
+            "[1, 0, 0, 0, 0]": "thumb",
         }
 
     def user_controls(self, raised_fingers):
-        for gesture, shellName in self.app_data["userDefinedControls"].items():
-            if self.gesture_map[gesture] == raised_fingers:
-                for app in self.data:
-                    if app["displayName"] == shellName:
-                        subprocess.run(app["shellName"], shell=True)
-                        break
+        if raised_fingers is not None and raised_fingers != [0, 0, 0, 0, 0]:
+            if raised_fingers == [0, 1, 0, 0, 0]:
+                subprocess.run(
+                    self.app_data["userDefinedControls"][
+                        self.gesture_map[str(raised_fingers)]
+                    ],
+                    shell=True,
+                )
+
+            elif raised_fingers == [0, 1, 1, 0, 0]:
+                subprocess.run(
+                    self.app_data["userDefinedControls"][
+                        self.gesture_map[str(raised_fingers)]
+                    ],
+                    shell=True,
+                )
+
+            elif raised_fingers == [0, 1, 1, 1, 0]:
+                subprocess.run(
+                    self.app_data["userDefinedControls"][
+                        self.gesture_map[str(raised_fingers)]
+                    ],
+                    shell=True,
+                )
+
+            elif raised_fingers == [0, 1, 1, 1, 1]:
+                subprocess.run(
+                    self.app_data["userDefinedControls"][
+                        self.gesture_map[str(raised_fingers)]
+                    ],
+                    shell=True,
+                )
+
+            elif raised_fingers == [1, 0, 0, 0, 0]:
+                subprocess.run(
+                    self.app_data["userDefinedControls"][
+                        self.gesture_map[str(raised_fingers)]
+                    ],
+                    shell=True,
+                )
